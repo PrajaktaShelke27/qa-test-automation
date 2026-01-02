9dbc54797a4b62fe7672f85850e8ff21d0e1efbf
Prajakta Shelke <prajakta.sp27@gmail.com>
2026-01-02 22:47:56 +0530

    Updated the element Page testcases

--- src/test/java/com/test/testcases/Ele_Page_Testcases.java
+++ src/test/java/com/test/testcases/Ele_Page_Testcases.java
@@ -148,7 +148,7 @@ public class Ele_Page_Testcases extends BaseClass {
 		System.out.println(acttitle);
 		Assert.assertEquals(acttitle, exptitle,"Title matches");
 	}
-	@Test
+	@Test(enabled=false)
 	public void validateHomeLink()
 	{
 		String actlink=EP.verifyHomeLinksBtn();
@@ -156,6 +156,109 @@ public class Ele_Page_Testcases extends BaseClass {
 		System.out.println(actlink);
 		Assert.assertEquals(actlink, explink,"Title matches");
 	}
+	
+	@Test(enabled=false)
+	public void validatesecodHomeLink()
+	{
+		String actlink=EP.verifysecondHomeLink();
+		String explink="https://demoqa.com/";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+	}
+	@Test(enabled=false)
+	public void validateAPILinkCreated()
+	{
+		String actlink=EP.verifyAPILinkcreated();
+		String explink="Link has responded with staus 201 and status text Created";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("201"));
+		Assert.assertTrue(actlink.contains("Created"));
+
+	}
+	
+	@Test(enabled=false)
+	public void validateAPILinkNoContent()
+	{
+		String actlink=EP.verifyAPILinkNoContent();
+		String explink="Link has responded with staus 204 and status text No Content";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("204"));
+		Assert.assertTrue(actlink.contains("No Content"));
+
+	}
+	@Test(enabled=false)
+	public void validateAPILinkMoved()
+	{
+		String actlink=EP.verifyAPILinkMoved();
+		String explink="Link has responded with staus 301 and status text Moved Permanently";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("301"));
+		Assert.assertTrue(actlink.contains("Moved Permanently"));
+
+	}
+	
+	@Test(enabled=false)
+	public void validateAPILinkBadReq()
+	{
+		String actlink=EP.verifyAPILinkBadReq();
+		String explink="Link has responded with staus 400 and status text Bad Request";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("400"));
+		Assert.assertTrue(actlink.contains("Bad Request"));
+
+	}
+	@Test(enabled=false)
+	public void validateAPILinkUnauthorized()
+	{
+		String actlink=EP.verifyAPILinkUnauthorized();
+		String explink="Link has responded with staus 401 and status text Unauthorized";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("401"));
+		Assert.assertTrue(actlink.contains("Unauthorized"));
+
+	}
+	@Test(enabled=false)
+	public void validateAPILinkForbidden()
+	{
+		String actlink=EP.verifyAPILinkForbidden();
+		String explink="Link has responded with staus 403 and status text Forbidden";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("403"));
+		Assert.assertTrue(actlink.contains("Forbidden"));
+
+	}
+	@Test(enabled=false)
+	public void validateAPILinknotFound()
+	{
+		String actlink=EP.verifyAPILinkNotfound();
+		String explink="Link has responded with staus 404 and status text Not Found";
+		System.out.println(actlink);
+		Assert.assertEquals(actlink, explink,"Title matches");
+		Assert.assertTrue(actlink.contains("404"));
+		Assert.assertTrue(actlink.contains("Not Found"));
+
+	}
+	
+	@Test(enabled=false) 	
+	public void ValidateBrokenlinks()
+	{
+		int brokenlink_count=EP.VerifykbrokenLink();
+		Assert.assertEquals(brokenlink_count, 1,"Broken Link found" +brokenlink_count);
+	}
+	
+	@Test
+	public void ValidateBrokenImgs()
+	{
+		int brokenImg_count=EP.VerifykbrokenImgs();
+		Assert.assertEquals(brokenImg_count, 1,"Broken Link found" +brokenImg_count);
+	}
+
 
 	@AfterMethod
 	public void tearDown() {
